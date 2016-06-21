49981c36612c7f4f022cc45c206a21cd34924e60
Kokarn <kokarn@gmail.com>
2016-06-21 10:57:36 +0200

    Add some basic handling of missing and/or invalid config files. Fixed #5

--- src/index.js
+++ src/index.js
@@ -7,16 +7,15 @@ const childProcess = require('child-process-promise');
 const express = require('express');
 
 const configPath = path.resolve(process.cwd(), process.argv.slice().pop());
-const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 const app = express();
 
-async function build (target, args) {
+let config = false;
+
+async function build (args) {
     const commands = args.commands;
     const options = {
         cwd: args.path,
     };
-
-    console.log(`Deploying ${ target }...`);
 
     for (let i = 0; i < commands.length; i++) {
         const date = (new Date()).toLocaleDateString('sv-SE', {
@@ -35,8 +34,8 @@ app.post('/', (req, res) => {
     const target = req.query.target;
 
     if (config.repos[target]) {
-        if (req.get('x-github-event') && req.get('x-github-event') === 'push') {
-            build(target, config.repos[target]);
+        if(req.get('HTTP_X_GITHUB_EVENT') && req.get('HTTP_X_GITHUB_EVENT') === 'push'){
+            build(config.repos[target]);
             res.status(200).send();
         } else {
             res.status(401).send();
@@ -46,4 +45,25 @@ app.post('/', (req, res) => {
     }
 });
 
-app.listen(config.port, config.host);
+if(configPath.length < 1 || configPath === __filename ){
+    console.error('Didnt\'t get a config file, are you sure you specified one?');
+    process.exit(1);
+}
+
+fs.access(configPath, fs.R_OK, (err) => {
+    if(err){
+        console.error('Unable to read file "', configPath, '". Please make sure it exists and is readable.');
+        process.exit(1);
+    }
+
+    try {
+        config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+    } catch (err){
+        console.error('Unable to parse config file "' + configPath + '". Please make sure it\'s valid.');
+        process.exit(1);
+    }
+
+    if(config){
+        app.listen(config.port, config.host);
+    }
+});
